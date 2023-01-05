f262f009d45429585e6046324713aea40a1d80fd
Abir Hasan <cse.170201063@gmail.com>
2023-01-06 05:05:10 +0600

    importing mail done

--- src/Pages/Imports.js
+++ src/Pages/Imports.js
@@ -1,24 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../authProvider/AuthProvider";
 
 const Imports = () => {
+    const [ importedEmail, setImportedEmail ] = useState([])
+        console.log(importedEmail);
   const { user } = useContext(AuthContext);
     console.log(user);
+    const processingCsv = (data) => {
+        // console.log(data);
+        const email = data.split('\n');
+        // console.log(email);
+        setImportedEmail(email)
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
         const csv = form.csvFile.files[0]
-        console.log(csv);
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const data = e.target.result;
+            // console.log(data);
+            processingCsv(data);
+
+        }
+        reader.readAsText(csv);
     }
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <form onSubmit={handleSubmit}>
-        <div>
-          <input type="file" name="csvFile" id="csvFile" accept=".csv" />
-          {/* <label for="csvFile">Choose File</label> */}
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
+        <input type="file" name="csvFile" id="csvFile" accept=".csv" />
+        {/* <label for="csvFile">Choose File</label> */}
+
+        <input
+          type="submit"
+          value="Import"
+          className="p-2 rounded bg-green-300"
+        />
+          </form>
+        {importedEmail.length >0 &&
+      <div className="mt-5">
+          {importedEmail.map((gmail) => <li className="text-xl" key={Math.random()}>{gmail}</li>)}
+        <button className="p-2 rounded bg-green-300 mt-5">Send Mail</button>
+      </div>
+          }
     </div>
   );
 };
